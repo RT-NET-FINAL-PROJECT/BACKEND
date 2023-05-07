f225e5f772b63568eac0af7f63618222b3e6d851
Frisy Novel <98008631+freezyyyyyy@users.noreply.github.com>
2023-05-07 16:06:51 +0700

    CRUD event dan payment

--- controllers/controllerEvent.js
+++ controllers/controllerEvent.js
@@ -2,13 +2,29 @@ const { Post } = require("../models");
 class ControllerEvent {
   static async findAllEvent(req, res, next) {
     try {
-      const events = await Post.findall({
+      let options = {
         where: {
           category: "event",
         },
-      });
+      };
+
+      const events = await Post.findall(options);
 
       res.status(200).json(events);
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  static async findAllAnnouncements(req, res, next) {
+    try {
+      const announcements = await Post.findall({
+        where: {
+          category: "pengumuman",
+        },
+      });
+
+      res.status(200).json(announcements);
     } catch (error) {
       next(error);
     }
@@ -29,15 +45,39 @@ class ControllerEvent {
 
   static async createEvent(req, res, next) {
     try {
-      const {name, deskripsi, kategori, lokasi, biaya} = req.body;
+      const { name, deskripsi, kategori, lokasi, biaya } = req.body;
+      const newEvent = await Post.create({
+        name,
+        rt_id: req.user.rt_id,
+        deskripsi,
+        kategori,
+        lokasi,
+        biaya,
+        isPaid: false,
+      });
 
-   } catch (error) {
+      res.status(201).json(newEvent);
+    } catch (error) {
       next(error);
     }
   }
 
   static async updateEvent(req, res, next) {
     try {
+      const { postId } = req.params;
+      const { name, deskripsi, kategori, lokasi, biaya } = req.body;
+
+      const event = await Post.findByPk(postId);
+      if (!event) throw { name: "POST_NOT_FOUND" };
+
+      await Post.update(
+        { name, deskripsi, kategori, lokasi, biaya },
+        { where: { id: event.id } }
+      );
+
+      const message = `Event with id ${event.id} has ben updated`;
+
+      res.status(200).json({ message });
     } catch (error) {
       next(error);
     }
@@ -45,6 +85,18 @@ class ControllerEvent {
 
   static async deleteEvent(req, res, next) {
     try {
+      const { postId } = req.params;
+
+      const deletedEvent = await Post.findByPk(postId);
+      if (!deletedEvent) throw { name: "POST_NOT_FOUND" };
+
+      await Post.destroy({
+        where: { id: postId },
+      });
+
+      let message = `Berhasil menghapus event ${deletedEvent}`;
+
+      res.status(200).json({ message });
     } catch (error) {
       next(error);
     }
--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -2,18 +2,24 @@ module.exports = async (error, req, res, next) => {
   let message, status;
 
   switch (error.name) {
-    case "SequelizeUniqueConstraintError":
-      message= error.errors[0].message;
+    case "SequelizeValidationError":
+      message = error.errors.map((el) => {
+        return (el = el.message);
+      });
       status = 400;
-      
+
+    case "SequelizeUniqueConstraintError":
+      message = error.errors[0].message;
+      status = 400;
+
     case "InvalidToken":
       message = "Invalid Token";
-      status = 401;  
+      status = 401;
       break;
-      
+
     case "POST_NOT_FOUND":
       message = "Post not found";
-      status = 404;  
+      status = 404;
       break;
 
     default:
