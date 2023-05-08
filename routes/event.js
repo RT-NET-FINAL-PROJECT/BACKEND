8c5a4e420a55da8fb26809915e78fb551f5c537b
Frisy Novel <98008631+freezyyyyyy@users.noreply.github.com>
2023-05-09 00:16:50 +0700

    test multer

--- routes/event.js
+++ routes/event.js
@@ -7,7 +7,6 @@ router.get("/event", authentication, ControllerEvent.findAllEvent);
 router.get("/pengumuman", authentication, ControllerEvent.findAllAnnouncements);
 router.post("/event", authentication,authorizationRt, ControllerEvent.createEvent);
 
-
 router.put("/event/:id",authentication,authorizationRt, ControllerEvent.updateEvent);
 router.get("/event/:id", authentication, ControllerEvent.detailEvent);
 router.delete("/event/:id", authentication,authorizationRt, ControllerEvent.deleteEvent);
