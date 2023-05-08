7587f835ca07412914af5888c1486caa805be0c1
Frisy Novel <98008631+freezyyyyyy@users.noreply.github.com>
2023-05-08 13:56:07 +0700

    kategori -> category

--- routes/index.js
+++ routes/index.js
@@ -3,23 +3,14 @@ const router = express.Router();
 const usersRouter = require('./warga')
 const eventRouter = require('./event')
 const paymentRouter = require('./payment')
-<<<<<<< HEAD
-const rtRouter = require("./rt");
-=======
 const rtRouter = require("./rt")
 const adminRouter = require("./admin")
->>>>>>> 84c152da36e527ff6b7872071c818154823e02ad
 const service = require("./service");
 
 router.use(usersRouter)
 router.use(eventRouter)
 router.use(paymentRouter)
 router.use(rtRouter)
-<<<<<<< HEAD
-router.use(service);
-
-=======
 router.use(adminRouter)
 router.use(service)
->>>>>>> 84c152da36e527ff6b7872071c818154823e02ad
 module.exports = router
