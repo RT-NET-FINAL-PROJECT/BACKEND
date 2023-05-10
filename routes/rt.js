787b8d183098d116d7778b4a65fd1729d325502b
Frisy Novel <98008631+freezyyyyyy@users.noreply.github.com>
2023-05-10 14:37:09 +0700

    user detail

--- routes/rt.js
+++ routes/rt.js
@@ -1,5 +1,6 @@
 const router = require('express').Router()
 const ControllerRt = require('../controllers/controllerRt')
+const ControllerUser = require('../controllers/controllerUser')
 const { login } = require('../controllers/controllerUser')
 const { authentication } = require('../middlewares/authentication')
 const { authorizationRt} = require('../middlewares/authorization')
@@ -16,6 +17,7 @@ router.delete('/rt/users/submissions/:id',authentication,authorizationRt, Contro
 // router.patch('/rt/users/:id',authentication,authorizationRt, ControllerRt.approveUser) //persetujuan rt agar bisa login
 
 router.get('/rt/users', authentication, authorizationRt, ControllerRt.findAllWarga) //yang bisa get all data warga hanya RT dan Sekertariat
+router.get('/rt/users/:id',authentication, ControllerUser.detailUser)//buat profile
 router.post('/rt/users', authentication, authorizationRt, ControllerRt.createWarga) //add warga sebatas main entity aja
 router.put('/rt/users/:id', authentication, authorizationRt, ControllerRt.updateWarga) //add warga sebatas main entity aja
 router.delete('/rt/users/:id', authentication, authorizationRt, ControllerRt.deleteWarga) //delete data warga by id
